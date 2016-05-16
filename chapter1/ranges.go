768523031c66d34196963a7c063aca873d8b71f1
daniel portella <dmportella@users.noreply.github.com>
2016-05-16 17:13:30 +0100

    updated more detail

--- chapter1/ranges.go
+++ chapter1/ranges.go
@@ -2,9 +2,12 @@ package chapter1
 
 import "fmt"
 
+// ranges can bu used to iterate over arrays, maps, slices and channels.
+// we will discuss channels at a later chapter.
 func Range() {
 	numbers:= [5]int{1, 2, 3, 4 ,5}
 
+	// simple use of a range over our numbers
 	for index, value := range numbers {
 		fmt.Println("index:", index, "value:", value)
 	}
